c5a9adf67190a4a94be8c628399788bb0c5281c5
satojkovic <satojkovic@gmail.com>
2022-01-02 21:51:23 +0900

    Add RandomCrop class

--- transform.py
+++ transform.py
@@ -1,4 +1,5 @@
 from skimage import transform
+import numpy as np
 
 
 class Rescale:
@@ -17,4 +18,22 @@ class Rescale:
             new_h, new_w = self.output_size
         new_h, new_w = int(new_h), int(new_w)
         img = transform.resize(image, (new_h, new_w))
+        return img
+
+
+class RandomCrop:
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+
+    def __call__(self, image):
+        h, w = image.shape[:2]
+        new_h, new_w = self.output_size
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+        img = image[top:top + new_h, left:left+new_w]
         return img
