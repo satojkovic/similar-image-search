0364245fbdd19bf6b2db87b856b465f388b9d3e5
satojkovic <satojkovic@gmail.com>
2022-01-02 21:44:01 +0900

    Add new file

---
+++ transform.py
@@ -0,0 +1,20 @@
+from skimage import transform
+
+
+class Rescale:
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, image):
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+        new_h, new_w = int(new_h), int(new_w)
+        img = transform.resize(image, (new_h, new_w))
+        return img
