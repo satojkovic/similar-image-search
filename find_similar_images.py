a1b6c2b5e5698ecc99a27ecf88e94d22595bcbea
satojkovic <satojkovic@gmail.com>
2022-06-06 22:48:32 +0900

    Print query and closest images

--- find_similar_images.py
+++ find_similar_images.py
@@ -1,7 +1,11 @@
+import os
+
 import fire
-from annoy import AnnoyIndex
 import numpy as np
+from annoy import AnnoyIndex
 from skimage import io
+
+from dataset import ImageDataset
 from transform import get_transforms
 
 
@@ -21,13 +25,18 @@ def get_single_image(query_image):
     return image
 
 
-def run(features_npy, metrics='angular', ntrees=50, topk=5):
+def run(features_npy, dataname, data_dir, metrics='angular', ntrees=50, topk=5):
     features = np.load(features_npy)
     ann = construct_ann_index(features, metrics, ntrees)
     query_index = np.random.randint(0, ann.get_n_items())
     closest_items = ann.get_nns_by_item(query_index, topk)
     closest_items = closest_items[1:]
 
+    data_path = os.path.join(data_dir, dataname)
+    ds = ImageDataset(data_path)
+    print('Closest images for {}:'.format(ds.image_names[query_index]))
+    for item_index in closest_items:
+        print(ds.image_names[item_index])
 
 if __name__ == '__main__':
     fire.Fire(run)
